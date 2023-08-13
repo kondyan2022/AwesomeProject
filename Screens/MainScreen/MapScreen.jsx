3e4a26ae85477c21dc5b0dc3629108ae733f2eaf
kondyan2022 <kondyan.asusnexus@gmail.com>
2023-08-13 23:37:11 +0300

    add map

--- Screens/MainScreen/MapScreen.jsx
+++ Screens/MainScreen/MapScreen.jsx
@@ -1,9 +1,29 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const MapScreen = ({ navigation }) => {
+const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>MapScreen</Text>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log("Map is ready")}
+        onRegionChange={() => console.log("Region change")}
+      >
+        <Marker
+          title="I am here"
+          coordinate={{ latitude: 37.78825, longitude: -122.4324 }}
+          description="Hello"
+        />
+      </MapView>
     </View>
   );
 };
@@ -11,9 +31,13 @@ const MapScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
